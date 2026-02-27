c4ff360371e7437b8fb67b4f62ec5471543f4b5e
ginaa07 <1123150124@global.ac.id>
2026-02-27 20:56:59 +0700

    update materialApp dengan initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -34,5 +34,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyCatalog(),
+        '/cart': (context) => const MyCart(),
+      },
+    );
   }
 }
