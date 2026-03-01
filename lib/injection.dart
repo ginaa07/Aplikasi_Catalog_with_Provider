0d303668256d6f58c05aa9669b2bc15b15c948a3
ginaa07 <1123150124@global.ac.id>
2026-03-02 01:15:18 +0700

    add return changeNotifierProvider in file injection.dart

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:aplikasicatalog/features/cart/presentation/providers/cart_provider.dart';
+import 'package:aplikasicatalog/main.dart';
+import 'features/cart/data/repositories/cart_repository_impl.dart';
+import 'features/cart/presentation/providers/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 
 // Mengatur semua dependency di satu tempat
 Widget buildApp() {
-final cartRepository = CartRepositoryImpl();
+  final cartRepository = CartRepositoryImpl();
+  
+  return ChangeNotifierProvider(
+    create: (context) => CartProvider(repository: cartRepository),
+    child: const MyApp(),
+  );
+}
