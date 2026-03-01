f364fd50b1c05146c19c08232360e0bede4e85e7
ginaa07 <1123150124@global.ac.id>
2026-03-02 00:21:42 +0700

    add void removeall and booll isInCart in CartProvider

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -3,14 +3,22 @@ import 'package:aplikasicatalog/features/cart/domain/entities/product.dart';
 import 'package:aplikasicatalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartProvider extends ChangeNotifier {
-final CartRepository _repository;
+  final CartRepository _repository;
 
-// Dependency Injection melalui constructor
-CartProvider({required CartRepository repository}) : _repository = repository;
+  // Dependency Injection melalui constructor
+  CartProvider({required CartRepository repository}) : _repository = repository;
 
-List<Product> get items => _repository.getCartItems();
+  List<Product> get items => _repository.getCartItems();
 
-void addItem(Product product) {
-_repository.addItem(product);
-notifyListeners();
+  void addItem(Product product) {
+    _repository.addItem(product);
+    notifyListeners();
+  }
+
+  void removeAll() {
+    _repository.removeAllItems();
+    notifyListeners();
+  }
+
+  bool isInCart(String productId) => _repository.isItemInCart(productId);
 }
