1041bdbe7ca8b74814e49ccbc9ee0367bfab1fe6
ginaa07 <1123150124@global.ac.id>
2026-03-02 00:50:15 +0700

    add return TextButton tambah in file add_button_widget

--- lib/features/cart/presentation/widgets/add_button_widget.dart
+++ lib/features/cart/presentation/widgets/add_button_widget.dart
@@ -3,14 +3,23 @@ import 'package:aplikasicatalog/features/cart/domain/entities/product.dart';
 import 'package:aplikasicatalog/features/cart/presentation/providers/cart_provider.dart';
 import 'package:provider/provider.dart';
 
-
 class AddButtonWidget extends StatelessWidget {
   final Product product;
   const AddButtonWidget({required this.product, super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Mengecek dari provider apakah produk dengan id ini ada di keranjang
     final isInCart = context.select<CartProvider, bool>(
       (provider) => provider.isInCart(product.id),
     );
+
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          : () => context.read<CartProvider>().addItem(product),
+      child: isInCart
+          ? const Icon(Icons.check, color: Colors.green)
+          : const Text('TAMBAH'),
+    );
+  }
+}
