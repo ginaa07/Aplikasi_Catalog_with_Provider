5dc11b17187b7ce428126e2033ceeaa3804c1dd2
ginaa07 <1123150124@global.ac.id>
2026-03-01 23:54:30 +0700

    add class CartRepositoriesImpl implements

---
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -0,0 +1,8 @@
+import 'package:aplikasicatalog/features/cart/domain/entities/product.dart';
+import 'package:aplikasicatalog/features/cart/domain/repositories/cart_repository.dart';
+
+// Implementasi konkret dari CartRepository
+// Di sini bisa diganti dengan API call, database, dll
+class CartRepositoryImpl implements CartRepository {
+  // Kita gunakan _items (private) agar data lebih aman sesuai standar OOP
+  final List<Product> _items = [];
