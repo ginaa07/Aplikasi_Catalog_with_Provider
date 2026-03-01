ce372fd737cf01a1483f770ad0c85e14284a2d42
ginaa07 <1123150124@global.ac.id>
2026-03-02 00:38:52 +0700

    addition of body: ListView .builder and itemcount, builder, etc

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:aplikasicatalog/features/cart/domain/entities/product.dart';
 
-
 class CatalogPage extends StatelessWidget {
-const CatalogPage({super.key});
+  const CatalogPage({super.key});
 
-static final List<Product> _products = [
-Product(id: '1', name: 'Nasi Goreng', price: 15000),
-Product(id: '2', name: 'Sate Ayam', price: 20000),
-Product(id: '3', name: 'Es Teh', price: 5000),
-];
+  static final List<Product> _products = [
+    Product(id: '1', name: 'Nasi Goreng', price: 15000),
+    Product(id: '2', name: 'Sate Ayam', price: 20000),
+    Product(id: '3', name: 'Es Teh', price: 5000),
+  ];
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Katalog Makanan'),
         actions: [
@@ -23,3 +22,16 @@ Widget build(BuildContext context) {
           ),
         ],
       ),
+
+      body: ListView.builder(
+        itemCount: _products.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(_products[index].name),
+          
+          subtitle: Text('Rp ${_products[index].price}'),
+          trailing: AddButtonWidget(product: _products[index]),
+        ),
+      ),
+    );
+  }
+}
