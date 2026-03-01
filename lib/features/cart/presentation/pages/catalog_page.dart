c1b7bb56c5d936d5150d608d3da664446fd8bfc3
ginaa07 <1123150124@global.ac.id>
2026-03-02 00:31:37 +0700

    create scaffold, appbar with actions iconButton Katalog Makanan

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -4,8 +4,22 @@ import 'package:aplikasicatalog/features/cart/domain/entities/product.dart';
 
 class CatalogPage extends StatelessWidget {
 const CatalogPage({super.key});
+
 static final List<Product> _products = [
 Product(id: '1', name: 'Nasi Goreng', price: 15000),
 Product(id: '2', name: 'Sate Ayam', price: 20000),
 Product(id: '3', name: 'Es Teh', price: 5000),
 ];
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+      appBar: AppBar(
+        title: const Text('Katalog Makanan'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () => Navigator.pushNamed(context, '/cart'),
+          ),
+        ],
+      ),
