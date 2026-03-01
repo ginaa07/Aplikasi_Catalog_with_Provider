104479710b5754becb1966856625f3825a71280e
ginaa07 <1123150124@global.ac.id>
2026-03-01 23:55:33 +0700

    add override list product getCartItems

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -6,3 +6,9 @@ import 'package:aplikasicatalog/features/cart/domain/repositories/cart_repositor
 class CartRepositoryImpl implements CartRepository {
   // Kita gunakan _items (private) agar data lebih aman sesuai standar OOP
   final List<Product> _items = [];
+
+  @override
+List<Product> getCartItems() => List.unmodifiable(_items);
+@override
+void addItem(Product product) => _items.add(product);
+@override
